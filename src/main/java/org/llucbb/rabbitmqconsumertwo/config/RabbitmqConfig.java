832d4283bf05210f63a89f3be04c35b529056b4f
llucbb <llucbb@gmail.com>
2020-04-18 17:40:47 +0200

    You Can't Control The Package Name

--- src/main/java/org/llucbb/rabbitmqconsumertwo/config/RabbitmqConfig.java
+++ src/main/java/org/llucbb/rabbitmqconsumertwo/config/RabbitmqConfig.java
@@ -22,4 +22,38 @@ public class RabbitmqConfig {
         return new Jackson2JsonMessageConverter();
     }
 
+
+    /**
+     * If you works with other system that already publish to RabbitMQ, chances are you cannot control the __TypeId__
+     * header (which is Spring specific). Maybe other publisher use different header name, like type. If it different
+     * header name exists (for example, type), second approach:
+     */
+    /*@Bean(name = "rabbitListenerContainerFactory")
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer, ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+
+        factory.setAfterReceivePostProcessors(new MessagePostProcessor() {
+
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                var type = message.getMessageProperties().getHeaders().get("type").toString();
+                String typeId = null;
+
+                if (StringUtils.equalsIgnoreCase(type, "invoice.paid")) {
+                    typeId = InvoicePaidMessage.class.getName();
+                } else if (StringUtils.equalsIgnoreCase(type, "invoice.created")) {
+                    typeId = InvoiceCreatedMessage.class.getName();
+                }
+
+                Optional.ofNullable(typeId).ifPresent(t -> message.getMessageProperties().setHeader("__TypeId__", t));
+
+                return message;
+            }
+
+        });
+
+        return factory;
+    }*/
 }
--- src/main/java/org/llucbb/rabbitmqconsumertwo/consumer/DummyConsumer.java
+++ src/main/java/org/llucbb/rabbitmqconsumertwo/consumer/DummyConsumer.java
@@ -14,4 +14,45 @@ public class DummyConsumer {
         log.info("{}", message);
     }
 
+    /**
+     * If you works with other system that already publish to RabbitMQ, chances are you cannot control the __TypeId__
+     * header (which is Spring specific). Maybe other publisher use different header name, like type. If it different
+     * header name exists (for example, type), first approach:
+     * <p>
+     * Using if-else in @RabbitListener, based on existing header. Adjust the header name if required, on @Header
+     * parameter
+     */
+    /*@RabbitListener(queues = "q.finance.invoice")
+    public void listenInvoiceCreated(@Payload String message, @Header(AmqpHeaders.DELIVERY_TAG) long tag,
+                                     @Header("type") String type) throws IOException {
+        if (StringUtils.equalsIgnoreCase(type, "invoice.paid")) {
+            log.info("Delegate to invoice paid handler");
+        } else if (StringUtils.equalsIgnoreCase(type, "invoice.created")) {
+            log.info("Delegate to invoice created handler");
+        } else {
+            log.info("Delegate to default handler");
+        }
+    }*/
+
+
+    /**
+     * If you works with other system that already publish to RabbitMQ, chances are you cannot control the __TypeId__
+     * header (which is Spring specific). Maybe other publisher use different header name, like type. If it different
+     * header name exists (for example, type), second approach:
+     */
+   /*@RabbitHandler
+    public void listenInvoiceCreated(InvoiceCreatedMessage message) {
+        log.info("Listening invoice created : {}", message);
+    }
+
+    @RabbitHandler
+    public void listenInvoicePaid(InvoicePaidMessage message) {
+        log.info("Listening invoice paid : {}", message);
+    }
+
+    @RabbitHandler(isDefault = true)
+    public void listenDefault(Message message) {
+        log.info("Default invoice listener : {}", message.getMessageProperties().getHeaders());
+    }*/
+
 }
